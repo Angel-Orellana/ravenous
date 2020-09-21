3cf6e0459fd69b937bbee54c980e6cfcaeec1430
Angel Orellana <angelorellana.cvi@gmail.com>
2020-09-21 16:16:08 -0300

    First commit in the ravenous repo

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Business.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component{
+    render(){
+        return (
+            <div className='BusinessList'>
+                {
+                this.props.businesses.map(business => {
+                    return <Business key={business.id} business={business}/>
+                })}
+            </div>
+        );
+    }
+}
+
+export default BusinessList;
---
+++ src/components/util/Yelp.js
@@ -0,0 +1,28 @@
+const apiKey = 'sBpdlclvsCsRXU39hjsoMokOiQL1I7Y8zfqaW7ga4oX3VE4-QCPPhNa9pd2dvnQEBfiILD7iihT2dSnjMsxbP-ETfYsDIgKW7RKH8trbWIPwdWa7bfoPpLCKzAlhX3Yx';
+
+const Yelp = {
+    search(term, location, sortBy){
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+        { headers: { Authorization: `Bearer ${apiKey}`}}).then(
+           response => { return response.json()} 
+        ).then( jsonResponse => {
+            if (jsonResponse.businesses){
+                return jsonResponse.businesses.map( business => {
+                    return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories.title,
+                    rating: business.rating,
+                    reviewCount: business.review_count}
+                });
+            }
+        });
+    }, 
+}
+
+export default Yelp; 
